5045b31cd9d3e95ba11d1b3e1674688469751aac
rvictor <rvictorsoliveira@gmail.com>
2017-09-22 10:32:16 -0300

    comentado

--- teste.cpp
+++ teste.cpp
@@ -7,11 +7,11 @@ int main(){
  
     Objeto *o1 = new Objeto();
 
-    o1->imprimeMatrizBase();
-    o1->espelhoXY();
-	o1->imprimeMatrizBase();
-	o1->espelhoYZ();
-	o1->imprimeMatrizBase();
+//    o1->imprimeMatrizBase();
+//    o1->espelhoXY();
+//	o1->imprimeMatrizBase();
+//	o1->espelhoYZ();
+//	o1->imprimeMatrizBase();
 
 
     return 0;
