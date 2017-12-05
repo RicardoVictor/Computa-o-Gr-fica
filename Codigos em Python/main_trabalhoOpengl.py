bf22677a74793e28d1b0880c135d32376e095346
RicardoVictor <rvictorsoliveira@gmail.com>
2017-12-05 17:13:09 -0300

    trabalhando com transformações de forma independente

--- Codigos em Python/main_trabalhoOpengl.py
+++ Codigos em Python/main_trabalhoOpengl.py
@@ -215,6 +215,11 @@ def Base():
 
     glEnd()
 
+def inicializa():
+    glLoadIdentity()
+    gluPerspective(45, (display[0]/display[1]), 0.1, 100.0)
+
+
 ''' Variaveis de entrada '''
 #glRotate(angle, x, y, z) 
 #gluPerspective(ang, aspect, zNear, zFar)
@@ -242,45 +247,50 @@ glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
 ''' Transformacoes '''
 ''' Material '''
+
 glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, [0.5, 0.5, 0.5, 1])
 glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, [0.5, 0.5, 0.5, 1])
 glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0.5, 0.5, 0.5, 1])
 glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50)
-glTranslatef(1, -7, -25)
+glTranslate(1, -7, -25)
 Objeto('objetos/stage.obj')
 
-glRotatef(90, 0, 1, 0)
-glTranslate(5, 1, 0)
-glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, [0, 0, 0.03, 1])
-glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, [0, 0, 0.03, 1])
+inicializa()
+glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, [0, 0, 0.1, 1])
+glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, [0, 0, 0.1, 1])
 glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0.5, 0.5, 0.5, 1])
 glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50)
+glTranslate(0, -3, -15)
+glRotate(90, 0, 1, 0)
+glScalef(0.7, 0.7, 0.7)
 Objeto('objetos/drums basic.obj')
 
-glTranslate(0, 2, -6)
-glRotatef(-90, 0, 1, 0)
+inicializa()
+glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, [0.05, 0.1, 0.05, 1])
+glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, [0.05, 0.1, 0.05, 1])
+glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0.5, 0.5, 0.5, 1])
+glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50)
+glTranslatef(-3, -14, -50)
+glScalef(0.055, 1, 0.055)
+Cilindro()
+
+inicializa()
 glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, [0.1, 0.1, 0.1, 1])
 glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, [0.1, 0.1, 0.1, 1])
 glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0.5, 0.5, 0.5, 1])
 glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50)
+glTranslate(-3, -2, -15)
+glScalef(0.7, 0.7, 0.7)
 Objeto('objetos/Synthesizer.obj')
 
-glTranslate(-2, 7, 0)
-glScale(4, 4, 4)
-glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, [0.5, 0.0, 0.0, 1])
-glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, [0.5, 0.0, 0.0, 1])
+inicializa()
+glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, [0.3, 0.0, 0.0, 1])
+glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, [0.3, 0.0, 0.0, 1])
 glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0.5, 0.5, 0.5, 1])
 glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50)
+glTranslatef(-2.8, 0.8, -10)
+glScalef(1.5, 1.5, 1.5)
 Objeto('objetos/Slayer logo.obj')
-
-glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, [0.1, 0.1, 0.1, 1])
-glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, [0.1, 0.1, 0.1, 1])
-glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0.5, 0.5, 0.5, 1])
-glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50)
-glRotatef(45, 0, 0, 1)
-glScalef(0.005, 0.1, 0.005)
-glTranslatef(-70, -30, -3)
-Cilindro()
 
 
 ''' Loop '''
